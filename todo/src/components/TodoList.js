d777f9906602a32650a3980983b83d3ee6d0c2fd
Smithface <kylewibblesmith@gmail.com>
2018-08-03 14:26:41 -0400

    added more details to TodoList
    counter takes care of keys for React map

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const TodoList = (props) => {
-
+  let counter = props.todos.length;
+  
   return (
     <ul>
       {/* props.todos will come in as an array of objects w/ .text & .completed */}
       {props.todos.map((todo, index) => {
         return (
-          <li>
-            <input type="text"/>
-            <span></span>
-            <button></button>
+          <li key={counter++}>
+            <input type="checkbox"/>
+            <span>{todo.text}</span>
+            <button>X</button>
           </li>
         );
       })}
