b2a7a1025a430a107afa1bc6b751e5826c142599
Smithface <kylewibblesmith@gmail.com>
2018-08-03 14:17:12 -0400

    wrote general layout of App.js

---
+++ todo/src/containers/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import NewTodo from '../components/NewTodo';
+import TodoList from '../components/TodoList';
+
+import '../styles/app.css';
+
+class App extends React.Component {
+  state = {
+    todosArr = [],
+  }
+
+  render() {
+    const itemsLeft = this.state.todosArr.length;
+
+
+    return (
+      <div className="app_container">
+        <div>
+          <p>To-Do</p>
+        </div>
+        <NewTodo />
+        <span><strong>{itemsLeft}</strong> item{itemsLeft === 1 ? '' : 's'} left</span>
+        {(todosArr.length !== 0) && <TodoList />}
+      </div>
+    );
+  }
+}
