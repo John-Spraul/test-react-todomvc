3980a9736720daf8ef6caac8eeffbf1b0a44ebae
Smithface <kylewibblesmith@gmail.com>
2018-08-03 14:36:35 -0400

    NewTodo can now add new todos to the array in App.js

--- todo/src/components/NewTodo.js
+++ todo/src/components/NewTodo.js
@@ -5,12 +5,23 @@ class NewTodo extends React.Component {
     newTodo: ''
   }
 
-  handleChange = () => {}
+  handleChange = (event) => {
+    this.setState({ newTodo: String(event.target.value) });
+  }
+
+  addTodo = (event) => {
+    const newTodo = '' + this.state.newTodo.trim();
+    event.preventDefault();
+    if (newTodo.length) {
+      this.setState({ newTodo: '' });
+      this.props.addTodo(newTodo);
+    }
+  }
 
   render() {
     return (
-      <form>
-        <input type="text"/>
+      <form onSubmit={(event) => {this.addTodo(event)}}>
+        <input placeholder="Add New Todo Item" type="text" autoFocus onChange={(event) => {this.handleChange(event)}}/>
         <button type="submit">+</button>
       </form>
     );
