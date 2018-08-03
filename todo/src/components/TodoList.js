663b931d953a6e506182484d3a1df8ec695f13c7
Smithface <kylewibblesmith@gmail.com>
2018-08-03 14:51:20 -0400

    added styling to TodoList

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const TodoList = (props) => {
   let counter = props.todos.length;
-  
   return (
-    <ul>
+    <ul className="box box-centered box-column">
       {/* props.todos will come in as an array of objects w/ .text & .completed */}
       {props.todos.map((todo, index) => {
         return (
-          <li className="todo-item" key={counter++}>
+          <li className="box box-todo todo-item" key={counter++}>
             <input type="checkbox" onChange={() => {props.toggleCompleted(index)}} checked={todo.completed} />
             <span className={todo.completed ? "item-completed" : null}>{todo.text}</span>
             <button className="button__remove-todo" onClick={() => {props.removeTodo(index)}}>X</button>
